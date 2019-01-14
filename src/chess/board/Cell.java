a34f8e898ff37383deece406d8d3758e8892995a
ysmolyar <yonismolyar1@gmail.com>
2019-01-13 19:09:11 -0500

    created chessboard and cell classes

--- src/chess/board/Cell.java
+++ src/chess/board/Cell.java
@@ -1,4 +1,40 @@
 package chess.board;
 
+import chess.pieces.Piece;
+
+/**
+ * Class representing one cell of a ChessBoard.
+ */
 public class Cell {
+    private int row;
+    private int col;
+    private Piece occupant;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.occupant = null;
+
+    }
+
+    public Piece getOccupant() {
+        return occupant;
+    }
+
+    public void setOccupant(Piece newPiece) {
+        this.occupant = newPiece;
+    }
+
+    public boolean isEmpty() {
+        return this.occupant == null;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
 }
--- src/chess/board/ChessBoard.java
+++ src/chess/board/ChessBoard.java
@@ -1,4 +1,23 @@
 package chess.board;
 
+/**
+ * Class representing a ChessBoard comprised of Cells.
+ */
 public class ChessBoard {
+    private final int boardWidth = 8;
+    private final int boardHeight = 8;
+    private Cell[][] grid = new Cell[boardHeight][boardWidth];
+
+    public ChessBoard() {
+        this.initGrid();
+    }
+
+    private void initGrid() {
+        for (int row = 0; row < boardHeight; row++) {
+            for (int col = 0; col < boardWidth; col++) {
+                grid[row][col] = new Cell(row, col);
+            }
+        }
+    }
+
 }
